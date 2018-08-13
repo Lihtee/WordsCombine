52029c0ede647eb450b46fc905327ffe7e0d1f40
Андрей Пролеев <andriocactus@gmail.com>
2018-08-13 19:41:50 +0500

    Init

---
+++ WordsCombine/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WordsCombine
+{
+    public static class Patterns
+    {
+        public static readonly string a = "";
+    }
+    public class Class1
+    {
+        
+    }
+
+    public class Operatinos
+    {
+        public List<string> Combine(string wordA, string wordB)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CommonString
+    {
+        public List<int> positions;
+        public string common;
+    }
+}
